24c5e4b11a70a0968938f21e9bc415e598f1b7af
selimk92 <selim.kucukgul@gmail.com>
2024-10-14 21:27:52 +0300

    first endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,6 +10,13 @@ func main() {
 
 	server := gin.Default()
 
+	server.GET("/events", myGetEvents)
+
 	server.Run(":8080")
+
+}
+
+func myGetEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
 
 }
