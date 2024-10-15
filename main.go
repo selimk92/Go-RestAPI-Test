1d2de220b47812e457b0c6c2d0b13067bf0112d4
selimk92 <selim.kucukgul@gmail.com>
2024-10-15 16:34:58 +0300

    db-creation and query added

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/db"
 	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", myGetEvents)
@@ -30,10 +31,13 @@ func createEvent(context *gin.Context) {
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't parse data"})
+		return
 	}
 
 	event.ID = 1
 	event.UserID = 1
+
+	event.Save()
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 
 }
